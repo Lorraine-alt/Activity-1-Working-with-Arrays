0bd96538bcf9539deae53586c9edb6a2daf53790
Lorraine-alt <lozanolorraine812@gmail.com>
2024-10-17 20:26:34 +0800

    Create arrayActivity.js

---
+++ arrayActivity.js
@@ -0,0 +1,8 @@
+let favSingers = ["Bruno Mars", "Lany", "Enhypen"]; 
+console.log(favSingers [0]);
+
+let fav Numbers = [3, 15, 21, 9);  
+console.log(favNumbers);
+
+let mixedArr = ["string", ["otherarray"] 123, true);
+console.log(mixedArr);
